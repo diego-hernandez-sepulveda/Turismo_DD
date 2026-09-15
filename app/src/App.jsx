6f45867269c6c1bffb046bc9e7aed0cd51b96080
Daniela-pmSK <daniela.ponce660@gmail.com>
2026-09-15 19:46:42 -0300

    Modificando front-end
    Card y Accordion para primer destino y actividades

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,19 +3,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
 
 function App() {
   return (
     <div className="p-5">
       <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Inicio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home">Destinos</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown title="Buscador" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -30,9 +32,33 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-      <h1>Hola Mundo prueba </h1>
-      <h2>Hola Mundo 67</h2>
-      <Button variant="primary">Boton a color</Button>
+      <h1>ChileTurist</h1>
+      <h3>El mejor país de Chile</h3>
+
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Descripción
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Descripción
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+      </Card.Body>
+    </Card>
 
     </div>
   );
